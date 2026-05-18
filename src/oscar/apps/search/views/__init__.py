86c5df660bebb88bfea476b5c175eb5e99e0cd5c
Jeffrey He <jeffreyhe@hotmail.com>
2026-05-18 15:56:36 +0800

    ini

--- src/oscar/apps/search/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from oscar.core.loading import get_class
-
-FacetedSearchView = get_class("search.views.search", "FacetedSearchView")
-CatalogueView = get_class("search.views.catalogue", "CatalogueView")
-ProductCategoryView = get_class("search.views.catalogue", "ProductCategoryView")
